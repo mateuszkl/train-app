9d6ff28697a2ac7a096de629d53c1224d93240c2
Mateusz Klepacki <Mateusz_Klepacki@epam.com>
2020-02-26 16:13:33 +0100

    route implementation in progress, added training-form and not-found components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 import {DashboardService} from './dashboard/dashboard.service';
+import {RouterModule} from '@angular/router';
+import {TrainingFormComponent} from './training/training-form/training-form.component';
+import {NotFoundComponent} from './not-found-component/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    DashboardComponent
+    DashboardComponent,
+    TrainingFormComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: DashboardComponent
+      },
+      {
+        path: 'new',
+        component: TrainingFormComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      },
+    ])
   ],
   providers: [
     DashboardService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
